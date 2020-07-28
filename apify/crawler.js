90541f7c582b3b5967d16fa6cfdf00343ae838e6
Eric <ehchao88@gmail.com>
2020-07-28 15:11:35 -0400

    Able to extract title, image, and readtime from html webpage. Read time not very accurate due to extraneous text in the html files. Picture is just the first image on the website which may not be correct

---
+++ apify/crawler.js
@@ -0,0 +1,29 @@
+const Apify = require('apify')
+
+async function testCrawl(url){
+    const requestQueue = await Apify.openRequestQueue();
+
+    await requestQueue.addRequest({url: url});
+
+    const handlePageFunction = async({ request, $ }) => {
+        const title = $('title').text();
+        const img = $('img').attr("src");
+        
+        //getting the word count of the webpage to calculate the read time
+        const len = $('body').text().split(' ').length;
+
+        //readtime calulcated by dividing word count by 200(average reading speed)
+        //depending on website article is from, it may be inaccurate because a lot of miscellaneous info is included
+        const readTime = len/200;
+        console.log(`The title of "${request.url}" is: ${title}.\nThe image is ${img}\nWord Count is ${len}
+        \nIt will take about ${readTime} minutes to read.`);
+    }
+
+    const crawler = new Apify.CheerioCrawler({
+        requestQueue,
+        handlePageFunction,
+    });
+
+    await crawler.run();
+}
+module.exports = testCrawl;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../auth');
+const crawler = require('../../apify/crawler')
 
 router.use('/users', require('./users'));
 router.use('/chat', require('./chat'))
+
+router.post('/add_article', auth.required, (req, res, next) => {
+    try{
+        crawler(req.body.article)
+    }
+    catch(err){
+        return res.send(err);
+    }
+    res.sendStatus(200);
+})
 
 module.exports = router;
