f929a484b573013780dc37b725c405c01b0cf402
Eric <ehchao88@gmail.com>
2020-12-10 22:43:57 -0500

    Individual convo user data

--- data-analysis/vanity.py
+++ data-analysis/vanity.py
@@ -10,6 +10,8 @@ import os.path
 from transcripts import create_transcript
 from bson.objectid import ObjectId
 from datetime import datetime
+from statistics import mean
+from collections import Counter
 import io
 import boto3
 import sys
@@ -36,13 +38,121 @@ def vanity(room_id: str):
     cur_room = rooms.find_one({"_id":ObjectId(room_id)})
     users = cur_room['users']
     usernames = []
+    user_ages = []
+    user_genders = []
+    user_loc = []
     mycursor = mydb.cursor()
     for user in users:
         mycursor.execute("SELECT username FROM test_server1.Users U WHERE U.id="+user)
         myresult = mycursor.fetchone()
         usernames.append(myresult[0])
-    #print(str(len(usernames))+ " users: " + str(usernames))
-    #print(os.path.exists('../transcripts/'+room_id+'_chat.csv'))
+        mycursor.execute("SELECT age FROM test_server1.Users as U WHERE U.id=" + user)
+        a = mycursor.fetchone()
+        if a != None:
+            user_ages.append(int(a[0]))
+        mycursor.execute("SELECT gender FROM test_server1.Users as U WHERE U.id=" + user)
+        g = mycursor.fetchone()
+        if g != None and str(g[0]) != "N/A":
+            user_genders.append(str(g[0]))
+        mycursor.execute("SELECT location FROM test_server1.Users AS U WHERE U.id=" + user)
+        l = mycursor.fetchone()
+        if l != None:
+            user_loc.append(str(l[0]))
+        for index, location in enumerate(user_loc):
+            if "Boston" in location or "boston" in location:
+                user_loc[index] = "Boston, MA"
+            if (location == "nyc") or ("New York City" in location) or ("New york city" in location) or ("new york city" in location):
+                user_loc[index] = "New York, NY" 
+            if ("United States".casefold() in location.casefold()):
+                user_loc[index] = "USA"
+        location_counts = Counter(user_loc)
+        location_list = sorted(location_counts, key = location_counts.get, reverse=True)
+    
+    #age donut chart
+    num_13_18 = 0
+    num_19_21 = 0
+    num_22_25 = 0
+    num_26_30 = 0
+    num_31_40 = 0
+    num_41_plus = 0
+    for index, age in enumerate(user_ages):
+        if age>=13 and age<= 18:
+            num_13_18 += 1
+        elif age>18 and age<=21:
+            num_19_21 += 1
+        elif age>21 and age<=25:
+            num_22_25 += 1
+        elif age>25 and age<=30:
+            num_26_30 += 1
+        elif age>30 and age<=40:
+            num_31_40 += 1
+        else:
+            num_41_plus += 1
+
+    total = len(user_ages)
+    percent_1318 = 0
+    percent_1921 = 0
+    percent_2225 = 0
+    percent_2630 = 0
+    percent_3140 = 0
+    percent_41p = 0
+
+    if(total > 0):
+        percent_1318 = num_13_18/total
+        percent_1921 = num_19_21/total
+        percent_2225 = num_22_25/total
+        percent_2630 = num_26_30/total
+        percent_3140 = num_31_40/total
+        percent_41p = num_41_plus/total
+
+    labels = ["13-18","19-21","22-25","26-30","31-40","41+"]
+    sizes = [percent_1318,percent_1921,percent_2225,percent_2630,percent_3140,percent_41p]
+    plt.pie(sizes, labels=labels, autopct = '%1.1f%%')
+    plt.axis('equal')
+
+    circle = plt.Circle(xy=(0,0), radius=0.75, facecolor='white')
+    plt.gca().add_artist(circle)
+    label = plt.gca().annotate("Average Age: "+str(round(mean(user_ages),1)), xy=(0,0), fontsize="10", ha="center")
+    img_data = io.BytesIO()
+    plt.savefig(img_data, format="svg")
+    img_data.seek(0)
+    bucket.put_object(Body=img_data, ContentType='image/svg+xml', Key="images/graphs/"+room_id+"_ageDonut.svg")
+    plt.clf()
+
+    #gender donut chart
+    #TODO: expand beyond binary male and female
+    num_male = 0
+    num_female = 0
+    for index, gender in enumerate(user_genders):
+        if gender.casefold() == "M".casefold() or gender.casefold() == "Male".casefold():
+            user_genders[index] = "Male"
+            num_male += 1
+        elif gender.casefold() == "F".casefold() or gender.casefold() == "Female".casefold():
+            user_genders[index] = "Female"
+            num_female += 1
+        else:
+            user_genders[index] = "Male"
+            num_male += 1
+    total = len(user_genders)
+    percent_male = 0
+    percent_female = 0
+
+    if(total > 0):
+        percent_male = num_male/len(user_genders)
+        percent_female = num_female/len(user_genders)
+    labels = ["Male","Female"]
+    sizes = [percent_male, percent_female]
+    plt.pie(sizes, labels=labels, autopct = '%1.1f%%')
+    plt.axis('equal')
+
+    circle = plt.Circle(xy=(0,0), radius=0.75, facecolor='white')
+    plt.gca().add_artist(circle)
+    img_data = io.BytesIO()
+    plt.savefig(img_data, format="svg")
+    img_data.seek(0)
+    bucket.put_object(Body=img_data, ContentType='image/svg+xml', Key="images/graphs/"+room_id+"_genderDonut.svg")
+    plt.clf()
+    #message vanity metrics
     if os.path.exists('transcripts/'+room_id+'_chat.csv') == False:
         #print("AHHHHHHH")
         create_transcript(room_id)
@@ -86,11 +196,14 @@ def vanity(room_id: str):
     img_data = io.BytesIO()
     plt.savefig(img_data, format="png")
     img_data.seek(0)
-    bucket.put_object(Body=img_data, ContentType='image/png', Key='images/graphs/'+room_id+'_wordcloud.png')
-    mycursor.execute("INSERT INTO test_server1.Analytics (id, totalMessages, totalWords, totalLetters, averageWordsMessage, averageLettersMessage, numUsers, convoLength, convoTime, convoDate)\
-        VALUES ('"+room_id+"'," + str(len(messages)) + ","+str(total_words)+","+str(total_chars)+","+str(avg_words)+","+str(avg_chars)+","+str(len(usernames))+","+str(t_delta)+",'"+str(convo_time)+"','"+str(convo_date)+"')")
+    bucket.put_object(Body=img_data, ContentType='image/png', Key='images/graphs/'+room_id+'_wordCloud.svg')
+    mycursor.execute("INSERT INTO test_server1.Analytics (id, totalMessages, totalWords, totalLetters, averageWordsMessage, averageLettersMessage, numUsers, convoLength, convoTime, convoDate, locations)\
+        VALUES ('"+room_id+"'," + str(len(messages)) + ","+str(total_words)+","+str(total_chars)+","+str(avg_words)+","+str(avg_chars)+","+str(len(usernames))+","+str(t_delta)+",'"+str(convo_time)+"','"+str(convo_date)+"','"+str(location_list).replace("'",'"')+"')")
     mydb.commit()
     #plt.show()
+    plt.clf()
+
+
     print(str({'total_chars':total_chars,'avg_chars':avg_chars,'total_words':total_words,'avg_words':avg_words, 'num_users':len(usernames)}))
 
 if __name__ == '__main__':
--- routes/analytics.js
+++ routes/analytics.js
@@ -13,14 +13,127 @@ router.get('/', auth.required, (req, res, next) => {
         where: {
             id: id
         }
+    }).then(async (user) => {
+        var userInfo = user.getUserInfo();
+        if (user.admin != "(Admin)"){
+            return res.sendStatus(401).json({"Err": "Unauthorized"})
+        }
+        var userInfo = user.getUserInfo();
+        if (user.admin != "(Admin)"){
+            return res.sendStatus(401).json({"Err": "Unauthorized"})
+        }
+        db.Analytics.findAll({
+            limit: 5,
+            order: [ ['createdAt', 'DESC']]
+        }).then(async (analyses) => {
+            let data = []
+            let articles = []
+            console.log("ANALYSES LENGth "+analyses.length+"\n"+analyses)
+            for (const analysis of analyses) {
+                let anal = await Room.findById(analysis.id);
+                console.log("AHHHHHHHH" + anal);
+                let art = await db.Article.findOne({
+                    where: {
+                        url: anal.url[0]
+                    }
+                })
+                console.log(art);
+                if(!art){
+                    try{
+                        crawler(anal.url[0])
+                    }
+                    catch(err){
+                        return res.send(err)
+                    }   
+                    return res.redirect('/analytics')                     
+                }
+                data.push(analysis.getData());
+                articles.push(art.getArticleInfo());
+            }
+            return res.render('analysis/dashboard', {user:user.username, data: data, article: articles})
+        })
+    })
+})
+
+router.get('/convos', auth.required, (req, res, next) => {
+    const { payload: { id } } = req;
+    var data = []
+    var articles = []
+    return db.User.findOne({
+        where: {
+            id: id
+        }
     }).then((user) => {
         var userInfo = user.getUserInfo();
         if (user.admin != "(Admin)"){
             return res.sendStatus(401).json({"Err": "Unauthorized"})
         }
-        return res.render('analysis/dashboard', {user:user.username})
+        db.Analytics.findAll({
+            limit: 5,
+            order: [ ['createdAt', 'DESC']]
+        }).then(async (analyses) => {
+            console.log("ANALYSES LENGth "+analyses.length+"\n"+analyses)
+            for (const analysis of analyses) {
+                let anal = await Room.findById(analysis.id);
+                console.log("AHHHHHHHH" + anal);
+                let art = await db.Article.findOne({
+                    where: {
+                        url: anal.url[0]
+                    }
+                })
+                console.log(art);
+                if(!art){
+                    if (!anal.url[0].includes("thesweatypengin.com")){
+                        try{
+                            crawler(anal.url[0])
+                        }
+                        catch(err){
+                            return res.send(err)
+                        }   
+                        return "err"   
+                    }                  
+                }
+                else{
+                    data.push(analysis.getData());
+                    articles.push(art.getArticleInfo());
+                }
+            }
+            return ({data:data, articles:articles})
+        }).then(async (result) => {
+            if (result=="err"){
+                res.redirect('/analytics/convos')
+            }
+            let allConvoArt = []
+            let allConvoData = []
+            let rooms = await Room.find();
+            for(let room of rooms){
+                console.log(room.url)
+                if(room.url[0].includes("thesweatypenguin.com")){
+                    continue
+                }
+                var roomData = await db.Analytics.findOne({
+                    where: {
+                        id: room._id.toString()
+                    }
+                })
+                var roomArt = await db.Article.findOne({
+                    where: {
+                        url: room.url[0] 
+                    }
+                })
+                if(roomArt && roomData){
+                    allConvoArt.push(roomArt.getArticleInfo())
+                    allConvoData.push(roomData.getData())
+                } 
+            }
+            console.log(result)
+            console.log(result.data)
+            console.log(result.articles)
+            return res.render('analysis/convo_splash', {user: user.getUserInfo().username, article: result.articles, data: result.data, allConvoArt: allConvoArt, allConvoData: allConvoData});
+        })
     })
 })
+
 
 router.get('/:chat', auth.required, (req, res, next) => {
     const { payload: { id } } = req;
@@ -38,6 +151,7 @@ router.get('/:chat', auth.required, (req, res, next) => {
                 id: req.params.chat
             }
         }).then((analysis) => {
+            //run python analysis if it has not already been run for this conversation
             if(!analysis){
                 var python = spawn('python3', ['./data-analysis/vanity.py', req.params.chat]);
                 python.stderr.on('data', function (data){
@@ -58,6 +172,7 @@ router.get('/:chat', auth.required, (req, res, next) => {
                 });
             }
             else{
+                //Find the room in the mongodb database
                 Room.findById(req.params.chat, (err, room) => {
                     if(err){
                         console.log(err);
@@ -68,6 +183,7 @@ router.get('/:chat', auth.required, (req, res, next) => {
                             url: room.url[0]
                         }
                     }).then((article) => {
+                        //if we have not already scraped this article, do so and refresh the page
                         if(!article){
                             try{
                                 crawler(room.url[0])
@@ -77,11 +193,122 @@ router.get('/:chat', auth.required, (req, res, next) => {
                             }
                             return res.redirect('/analytics/'+req.params.chat)                          
                         }
-                        return res.render('analysis/convo', {user: user.getUserInfo().username, article: article.getArticleInfo(), data: analysis.getData(), room:req.params.chat})
+                        //get the 5 most recent conversation for sidebar
+                        return db.Analytics.findAll({
+                            limit: 5,
+                            order: [ ['createdAt', 'DESC']]}).then(async (analyses) =>{
+                                let datas = []
+                        let articles = []
+                        console.log("ANALYSES LENGth "+analyses.length+"\n"+analyses)
+                        for (const analysis of analyses) {
+                            let anal = await Room.findById(analysis.id);
+                            console.log("AHHHHHHHH" + anal);
+                            let art = await db.Article.findOne({
+                                where: {
+                                    url: anal.url[0]
+                                }
+                            })
+                            console.log(art);
+                            //if article hasn't been scraped, then do so
+                            if(!art){
+                                try{
+                                    crawler(anal.url[0])
+                                }
+                                catch(err){
+                                    return res.send(err)
+                                }   
+                                return res.redirect('/analytics/'+req.params.chat)                     
+                            }
+                            
+                            datas.push(analysis.getData());
+                            articles.push(art.getArticleInfo());
+                        }
+                        return res.render('analysis/convo', {id:req.params.chat, user: user.getUserInfo().username, article: article.getArticleInfo(), data: analysis.getData(), room:req.params.chat, datas: datas, articles: articles})
+                            })
                     })
                 })
             }
         })
     })
 })
+
+router.get('/:chat/user', auth.required, (req,res,next) => {
+    const { payload: { id } } = req;
+    return db.User.findOne({
+        where:{
+            id: id
+        }
+    }).then((user) => {
+        if(user.admin != '(Admin)'){
+            return res.sendStatus(401).json({"Err": "Unauthorized"})
+        }
+        db.Analytics.findOne({
+            where: {
+                id: req.params.chat
+            }
+        }).then((analysis) => {
+            if (!analysis){
+                return res.redirect('/analytics/'+req.params.chat)
+            }
+            else{
+                //Find the room in the mongodb database
+                Room.findById(req.params.chat, (err, room) => {
+                    if(err){
+                        console.log(err);
+                        return res.send(err);
+                    }
+                    db.Article.findOne({
+                        where: {
+                            url: room.url[0]
+                        }
+                    }).then((article) => {
+                        //if we have not already scraped this article, do so and refresh the page
+                        if(!article){
+                            try{
+                                crawler(room.url[0])
+                            }
+                            catch(err){
+                                return res.send(err)
+                            }
+                            return res.redirect('/analytics/'+req.params.chat)                          
+                        }
+                        //get the 5 most recent conversation for sidebar
+                        return db.Analytics.findAll({
+                            limit: 5,
+                            order: [ ['createdAt', 'DESC']]}).then(async (analyses) =>{
+                                let datas = []
+                        let articles = []
+                        console.log("ANALYSES LENGth "+analyses.length+"\n"+analyses)
+                        for (const analysis of analyses) {
+                            let anal = await Room.findById(analysis.id);
+                            console.log("AHHHHHHHH" + anal);
+                            let art = await db.Article.findOne({
+                                where: {
+                                    url: anal.url[0]
+                                }
+                            })
+                            console.log(art);
+                            //if article hasn't been scraped, then do so
+                            if(!art){
+                                try{
+                                    crawler(anal.url[0])
+                                }
+                                catch(err){
+                                    return res.send(err)
+                                }   
+                                return res.redirect('/analytics/'+req.params.chat)                     
+                            }
+                            
+                            datas.push(analysis.getData());
+                            articles.push(art.getArticleInfo());
+                        }
+                        return res.render('analysis/user_ind', {id: req.params.chat, user: user.getUserInfo().username, article: article.getArticleInfo(), data: analysis.getData(), room:req.params.chat, datas: datas, articles: articles})
+                            })
+                    })
+                })
+            }
+        })
+    })
+})
+
 module.exports = router;
