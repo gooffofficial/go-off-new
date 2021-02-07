23915b829d0ee4406405a7ecebddb2b6f3fbbc89
leachs18 <samleach2018@gmail.com>
2021-02-06 20:26:44 -0500

    a variety of changes

--- models/UserArticles.js
+++ models/UserArticles.js
@@ -9,14 +9,16 @@ module.exports = (sequelize, type) => {
         },
         article1: type.STRING,
         article2: type.STRING,
-        article3: type.STRING
+        article3: type.STRING,
+        article4: type.STRING
     });
     
     UserArticle.prototype.getArticles = function(){
         return {
             article1: this.article1,
             article2: this.article2,
-            article3: this.article3
+            article3: this.article3,
+            article4: this.article4
          }
     }
     return UserArticle;
--- routes/api/users.js
+++ routes/api/users.js
@@ -550,7 +550,11 @@ router.get('/articles/:user', auth.optional, (req, res, next) => {
           article3Url: "",
           article3Title: "No article added",
           article3Readtime: "",
-          article3img: "/Plus.png"   
+          article3img: "/Plus.png" , 
+          article4Url: "",
+          article4Title: "No article added",
+          article4Readtime: "",
+          article4img: "/Plus.png"   
         })
       }
       var articleLinks = userArts.getArticles();
@@ -609,9 +613,28 @@ router.get('/articles/:user', auth.optional, (req, res, next) => {
           articles["article3Readtime"] = article3Info["readTime"]
           articles["article3img"] = article3Info["img"];
         }
+        db.Article.findOne({
+          where: {
+            url: articleLinks["article4"]
+          }
+        }).then((article4) => {
+          if(!article4){
+            articles["article4Url"] = "";
+            articles["article4Title"] = "No article added";
+            articles["article4Readtime"] = "";
+            articles["article4img"] = "/Plus.png";
+          }
+          else{
+            var article4Info = article4.getArticleInfo();
+            articles["article4Url"] = article4Info["url"];
+            articles["article4Title"] = article4Info["title"];
+            articles["article4Readtime"] = article4Info["readTime"]
+            articles["article4img"] = article4Info["img"];
+          }
         return res.status(200).json(articles);
         })
         })
+      })
       })
     })
   })
