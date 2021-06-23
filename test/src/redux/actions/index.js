c61d931448930980df4322f38fa3d19fe2e04dae
Erick Canales <e94canales@gmail.com>
2021-06-23 13:23:16 -0700

    added comments to redux files

--- test/src/redux/actions/index.js
+++ test/src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import * as globalActions from './globalActions';
 import * as signupactions from './signupactions';
 
-//global file taht all actions go through and are exported through
+// for organization and ease of imports and exports, we have a file that exports the action from one place
+// if a new action file is created, make sure to import it in here and add it to the exports below
 export { globalActions, signupactions };
-
--- test/src/redux/reducer/index.js
+++ test/src/redux/reducer/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { globalReducer } from './globalReducer';
-import { signupreducers }from './signupreducers';
+import { signupreducers } from './signupreducers';
 
+// for organization and ease of imports and exports, we have a file that combines all the reducers into one
+// if a new reducer is created, make sure to import it in here and add it to the exports below
 export default combineReducers({
-	global: globalReducer, 
+	global: globalReducer,
 	signupreducers: signupreducers,
-
 });
