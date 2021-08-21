170b47b352f96716e71cd8bced61ce1ff97e2a6b
leachs18 <samleach2018@gmail.com>
2021-08-21 09:41:12 -0400

    config tweak

--- client/src/config.js
+++ client/src/config.js
@@ -5,5 +5,7 @@ export default {
     storageBucket: "go-off-73642.appspot.com",
     messagingSenderId: "875727978225",
     appId: "1:875727978225:web:dc46e4a58ef64dfa57f350",
-    measurementId: "G-X40VQM29P7"
+    measurementId: "G-X40VQM29P7",
+    pubKey: "pub-c-b2e4c3f0-b22d-4947-ba1b-b36b8bd7bc32",
+    subKey: "sub-c-08c4cbbe-01f5-11ec-be1c-0664d1b72b66"
   };
