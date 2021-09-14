7b1c7b6f821649064d5a60e9a45bbf6fcc784371
Christian Nava <cnava9389@gmail.com>
2021-09-14 15:32:14 -0400

    fixing react

--- front/client/src/redux/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { globalReducer } from './globalReducer';
-import { signupreducers } from './signupreducers';
-
-export default combineReducers({
-	global: globalReducer,
-	signupreducers: signupreducers,
-});
