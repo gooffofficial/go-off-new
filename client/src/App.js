8f5ab73d6ab764d95a2d1b99df8495855770b349
Christian Nava <cnava9389@gmail.com>
2021-09-14 15:27:36 -0400

    fixing react

---
+++ client/src/App.js
@@ -0,0 +1,116 @@
+// Module Imports
+import { Switch, Route, useLocation, useHistory } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import "./styles/index.scss";
+import PubNub from "pubnub";
+import { PubNubProvider } from "pubnub-react";
+import { v4 as uuid_v4 } from "uuid";
+import config from "./config";
+
+// Component Imports
+import Home from "./pages/Home";
+import HostHome from "./pages/HostHome";
+import Profile from "./pages/Profile";
+import AccountSettingsPage from "./pages/AccountSettingsPage";
+import LiveChat from "./pages/LiveChat";
+import EditProfilePage from "./pages/EditProfilePage";
+import DiscoverPage from "./pages/DiscoverPage";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Sform from "./pages/sform";
+import Cform from "./pages/cform";
+import Ver from "./pages/ver";
+import Eauth from "./pages/eauth";
+import SMSauth from "./pages/smsauth";
+import Splash from "./pages/splash";
+import PublicProfile from "./pages/PublicProfile";
+import HostRoute from "./components/HostRoute";
+// import Log from '../../../apify/types/utils_log';
+import { useAuth0 } from "@auth0/auth0-react";
+import { UserContext } from "./contexts/userContext";
+import { routeContext } from "./contexts/useReroute";
+import SettingModal from "./components/SettingModal";
+
+const App = () => {
+  const { currentUser, isLoading, modal } = useContext(UserContext);
+  const { currentLocation, setCurrentLocation } = useContext(routeContext);
+  let history = useHistory();
+
+
+  // This will create a unique pubnub client with sub and pub keys. These are test keys we will need to buy full feature ones.
+  const pubnub = new PubNub({
+    publishKey: config.pubKey,
+    subscribeKey: config.subKey,
+    //should not generate new one each time should create one for the user upon account creation and use that.
+    //logVerbosity:true // logs HTTP request info
+  });
+
+  let location = useLocation().pathname;
+  useEffect(() => {
+    if (
+      !currentUser.signedIn &&
+      (location !== "/login" ||
+        location !== "/signup" ||
+        location !== "/" ||
+        location !== "/signup/" ||
+        location !== "/signup/ver" ||
+        location !== "/signup/smsauth" ||
+        location !== "/signup/eauth" ||
+        location !== "/signup/cform" ||
+        location !== "/signup/sform" ||
+        location !== "/404ERROR")
+    ) {
+      setCurrentLocation(location);
+      history.push("/");
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <div style={{ height: "20vh" }}></div>
+        <div className="row d-flex justify-content-center">
+          <div
+            style={{ width: "5rem", height: "5rem" }}
+            class="spinner-border text-primary"
+            role="status"
+          >
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    // Routes
+    <PubNubProvider client={pubnub}>
+      <Switch>
+        <Route path="/hosthome" component={HostHome} />
+        <Route path="/home" component={Home} />
+        {/* <HostRoute path="/home" component={HostHome} /> */}
+        <Route path="/profile/:username" component={PublicProfile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/accountsettings" component={AccountSettingsPage} />
+        <Route path="/editprofile" component={EditProfilePage} />
+        {/* <Route path="/discover" component={DiscoverPage} /> */}
+        <Route path="/chat/:code?" component={LiveChat} />
+        <Route path="/signup/eauth" component={Eauth} />
+        <Route path="/signup/smsauth" component={SMSauth} />
+        <Route path="/signup/ver" component={Ver} />
+        <Route path="/signup/cform" component={Cform} />
+        <Route path="/signup/sform" component={Sform} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route exact path="/" component={Splash} />
+        <Route exact path="/404ERROR">
+          <div>Oops! Page Not Found</div>
+        </Route>
+      </Switch>
+      <SettingModal show={modal} />
+    </PubNubProvider>
+  );
+};
+
+export default App;
---
+++ client/src/styles/AuthPage/api.js
@@ -0,0 +1,150 @@
+
+import axios from 'axios'
+import firebase from '../../firebase.js'
+
+const db = firebase.firestore();
+const getCurrentDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
+  const hour = today.getHours();
+  const minute = today.getMinutes();
+  const second = today.getSeconds();
+  return `${year}-${month<10?'0'+month:month}-${day<10?'0'+day:day} ${hour<10?'0'+hour:hour}:${minute<10?'0'+minute:minute}:${second<10?'0'+second:second}`
+}
+
+// let isLocalhost = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'; 
+// export let API_LINK = isLocalhost ? 'http://localhost:8000' : 'PRODUCTION_SITE_LINK_HERE'
+
+// export const sendEmailRegister = async (userInfo) => {
+//   try {
+//     let axiosResponse = await axios.post(`/api/users/ecreate`, userInfo)
+//     let serverResponse = axiosResponse.data // serverResponse returns us a huge HTML file that we dont need to use
+//     console.log("sendEmailRegister: ", serverResponse)
+//   } catch (err) { 
+//     console.log("error: ", err) 
+//   }
+// }
+
+export const sendEmailRegister = async (userInfo) => {
+  let errorResponse = {}
+  try {
+    let axiosResponse = await axios.post(`/api/users/ecreate`, userInfo)
+    let serverResponse = axiosResponse.data 
+
+    if (serverResponse.error) {
+      errorResponse = serverResponse;
+      throw serverResponse
+    }
+    console.log("sendEmailRegister: ", serverResponse)
+    return { res: serverResponse, isError: false }
+  } catch (err) { 
+    errorResponse = isEmpty(errorResponse) ? err.response.data : errorResponse;
+    console.log("Error: ", errorResponse) 
+    return { res: errorResponse, isError: true }
+  }
+}
+
+export const sendSMSRegister = async (userInfo) => {
+  let errorResponse = {}
+  try {
+    let axiosResponse = await axios.post(`/api/users/screate`, userInfo)
+    let serverResponse = axiosResponse.data 
+
+    if (serverResponse.error) {
+      errorResponse = serverResponse;
+      throw serverResponse
+    }
+    console.log("sendSMSRegister: ", serverResponse)
+    return { res: serverResponse, isError: false }
+  } catch (err) { 
+    errorResponse = isEmpty(errorResponse) ? err.response.data : errorResponse;
+    console.log("Error: ", errorResponse) 
+    return { res: errorResponse, isError: true }
+  }
+}
+
+export const sendVerifyCheck = async (email, verifyCode) => {
+  let errorResponse = {}
+  try {
+    let axiosResponse = await axios.get(`/api/users/verification?email=${email}&smscode=${verifyCode}`)
+    let serverResponse = axiosResponse.data 
+
+    if (serverResponse.error) {
+      errorResponse = serverResponse;
+      throw serverResponse
+    }
+    console.log("sendVerifyCheck: ", serverResponse)
+    return { res: serverResponse, isError: false }
+  } catch (err) { 
+    errorResponse = isEmpty(errorResponse) ? err.response.data : errorResponse;
+    console.log("Error: ", errorResponse) 
+    return { res: errorResponse, isError: true }
+  }
+}
+
+// export const sendVerifyCheck = async (email, verifyCode) => {
+//   try {
+//     let axiosResponse = await axios.get(`/api/users/verification?email=${email}&smscode=${verifyCode}`)
+//     let serverResponse = axiosResponse.data 
+//     return true;
+//   } catch (err) { console.log("error", err); return false; }
+// }
+
+export const getUpcomingChats = async (username = "") => { 
+  if (username === "") return (await axios.get(`/api/upcoming`)).data
+  else return (await axios.get(`/api/upcoming/${username}`)).data
+}
+export const getAllUpcomingChats = async (username) => (await axios.get('/api/getconvos')).data;
+
+export const getPastChats = async (username = "") => { 
+  if (username === "") return (await axios.get(`/api/pastconv`)).data;
+  else return (await axios.get(`/api/pastconv/${username}`)).data;
+}
+
+export const charLimit = (text, charMaxLength) => {
+  console.log(text)
+  return text.length > charMaxLength ? text.slice(0, charMaxLength) + "..." : text
+  
+}
+
+export const sendEditProf = async (editProfInfo, id) => (await axios.get('/api/users/update')).data;
+
+export const sendCreateConv = async (convCreationInfo,userId) => {
+  const { articleURL, time, title, description } = convCreationInfo; //*!description is the same as articleURL when
+  const convTime = new Date(time)
+  const currentDate= getCurrentDate()
+  const infoSent = { 
+    article: articleURL, 
+    convoTime: convTime.getTime() + "", 
+    // convoTime: time,
+    convoTitle: title,
+    convoDesc: description,
+    tz: "",
+    roomId: Math.floor(1000000000 + Math.random() * 9000000000),
+  }
+  const result = await axios.post(`${process.env.REACT_APP_FLASKAPI}/createConvo`,{
+    convoId:infoSent.roomId,
+    title:title,
+    description:description,
+    articleURL:articleURL,
+    hostId:userId,
+    isOpen:false,
+    time:convTime.getTime() + "",
+    ended:false,
+    createdAt:currentDate,
+    updatedAt:currentDate,
+    tz:0
+  }, {withCredentials:true})
+
+  if(result.status==200){
+    console.log('created convo!')
+  }else{
+    console.log('error creating convo')
+  }
+  
+  return (await axios.post(`/api/convos/create`, infoSent)).data; //*!not working
+}
+
+const isEmpty = (obj) => obj && Object.keys(obj).length === 0 && obj.constructor === Object
