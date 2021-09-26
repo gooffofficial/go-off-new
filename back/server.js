fe195779e52e3a9787a92a4c7fba7c0d63643e4a
cnava9389 <cnava9389@gmail.com>
2021-09-26 06:31:52 -0500

    fixed most issues

--- back/server.js
+++ back/server.js
@@ -39,7 +39,7 @@ app.set('view engine', 'ejs');
 app.use((req, res, next) => {
     console.log(req.hostname);
     const allowedOrigins = ['http://0.0.0.0:8000', 'http://0.0.0.0:3000', 'http://0.0.0.0:8080', 'http://0.0.0.0:4050','http://localhost:3000',
-    'http://www.go-of.co','https://www.go-of.co','http://go-of.co','http://128.199.11.144:3000'];
+    'http://www.go-of.co','https://www.go-of.co','http://go-of.co','http://128.199.11.144:8080','http://localhost:8080'];
     // const allowedOrigins = ['http://localhost:8000', 'http://localhost:3000, http://localhost:8080'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
